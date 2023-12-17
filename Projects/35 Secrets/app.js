7a7064eb3c29d294bbec40f18e715a7fe24e4c66
unknown <priyanshkumar001@gmail.com>
2023-12-18 01:45:02 +0530

    Level 5 Security, Cookies and Sessions using passport package, Section 35

--- Projects/35 Secrets/app.js
+++ Projects/35 Secrets/app.js
@@ -5,86 +5,104 @@ import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
 import md5 from 'md5';
 import bcrypt from 'bcrypt';
+import session from 'express-session';
+import passport from 'passport';
+import passportLocalMongoose from 'passport-local-mongoose';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: "My Session Secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connecting Mongoose database
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 // Creating Schema to send to database
-const userSchema = new mongoose.Schema( {
-    email : String,
-    password : String
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
 });
-
-// Encryption of password using a Secret.
-// userSchema.plugin(encrypt,{ secret: process.env.SECRET , encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
 
 // Creating a Model from the schema.
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.get("/", (req, res)=>{
-    res.render("home.ejs");
+
+app.get("/", (req, res) => {
+    if(!req.isAuthenticated())  res.render("home.ejs");
+    else res.redirect("/secrets");
 })
 
-app.get("/register", (req, res)=>{
-    res.render("register.ejs");
+app.get("/register", (req, res) => {
+    if(!req.isAuthenticated())  res.render("register.ejs");
+    else res.redirect("/secrets");
 })
 
-app.get("/login", (req, res)=>{
-    res.render("login.ejs");
+app.get("/login", (req, res) => {
+
+    if(!req.isAuthenticated())  res.render("login.ejs");
+    else res.redirect("/secrets");
 })
 
-app.post("/register", (req, res)=>{
-    bcrypt.hash(req.body.password, parseInt(process.env.SALTROUNDS), (err, hash)=>{
-        if(err) console.log(err.message);
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) res.render("secrets.ejs");
+    else res.redirect("/");
+})
 
-        const newUser = new User({
-            email : req.body.username,
-            password : hash                        // Storing the hashed password in DB instead of plain text.
-            // password : md5(req.body.password)   // Hashing the password.
+app.get("/logout", (req, res)=>{
+    if(req.isAuthenticated())   
+        req.logout(function(err){
+            if(err) console.log(err.message);
+            else res.redirect("/");
         });
-    
-        try{
-            newUser.save();
-    
-            res.render("secrets.ejs");
-        }catch(err){
+    else res.redirect("/");
+})
+
+app.post("/register", (req, res) => {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            });
         }
     });
 })
 
 
-app.post("/login",async (req, res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-    // const password = md5(req.body.password);   // Hashing the password.
+app.post("/login", async (req, res) => {
+    
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
+    });
 
-    try{
-        const foundUser = await User.where({email:username}).findOne();
-
-        if(foundUser){
-            bcrypt.compare(password, foundUser.password, (err, result)=>{
-                if(err) console.log(err.message);
-
-                if(result) res.render("secrets.ejs");
-                else console.log("username or password incorrect.");
-            })
+    req.login(user, function(err){
+        if(err) {
+            console.log(err.message);
+            res.redirect("/login");
         }
-        else console.log("username or password incorrect.")
-    }catch(err){
-        console.log(err);
-    }
+        else res.redirect("/secrets");
+    });
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server running on http://localhost:${3000}`);
 })
