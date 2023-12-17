7c7d880af69ddf1647f0b9740ae57c37c96a83b3
unknown <priyanshkumar001@gmail.com>
2023-12-17 23:10:35 +0530

    Level 4 Security Salt and Hash using bcrypt, Section 35

--- Projects/35 Secrets/app.js
+++ Projects/35 Secrets/app.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
 import md5 from 'md5';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const port = 3000;
@@ -41,30 +42,43 @@ app.get("/login", (req, res)=>{
 })
 
 app.post("/register", (req, res)=>{
-    const newUser = new User({
-        email : req.body.username,
-        password : md5(req.body.password)   // Hashing the password.
+    bcrypt.hash(req.body.password, parseInt(process.env.SALTROUNDS), (err, hash)=>{
+        if(err) console.log(err.message);
+
+        const newUser = new User({
+            email : req.body.username,
+            password : hash                        // Storing the hashed password in DB instead of plain text.
+            // password : md5(req.body.password)   // Hashing the password.
+        });
+    
+        try{
+            newUser.save();
+    
+            res.render("secrets.ejs");
+        }catch(err){
+            console.log(err);
+        }
     });
-
-    try{
-        newUser.save();
-
-        res.render("secrets.ejs");
-    }catch(err){
-        console.log(err);
-    }
 })
 
 
 app.post("/login",async (req, res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);   // Hashing the password.
+    const password = req.body.password;
+    // const password = md5(req.body.password);   // Hashing the password.
 
     try{
         const foundUser = await User.where({email:username}).findOne();
 
-        if(foundUser && foundUser.password === password) res.render("secrets.ejs");
-        else console.log("Username or Password is wrong");
+        if(foundUser){
+            bcrypt.compare(password, foundUser.password, (err, result)=>{
+                if(err) console.log(err.message);
+
+                if(result) res.render("secrets.ejs");
+                else console.log("username or password incorrect.");
+            })
+        }
+        else console.log("username or password incorrect.")
     }catch(err){
         console.log(err);
     }
