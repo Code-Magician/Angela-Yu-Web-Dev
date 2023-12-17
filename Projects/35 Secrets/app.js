3e244c3f266010d9a6022afeb43676b83914d02c
unknown <priyanshkumar001@gmail.com>
2023-12-17 19:44:35 +0530

    Completed Level 1 Security, Section 35

--- Projects/35 Secrets/app.js
+++ Projects/35 Secrets/app.js
@@ -1,14 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
-
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+const userSchema = {
+    email : String,
+    password : String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 
 app.get("/", (req, res)=>{
     res.render("home.ejs");
@@ -20,6 +30,36 @@ app.get("/register", (req, res)=>{
 
 app.get("/login", (req, res)=>{
     res.render("login.ejs");
+})
+
+app.post("/register", (req, res)=>{
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+
+    try{
+        newUser.save();
+
+        res.render("secrets.ejs");
+    }catch(err){
+        console.log(err);
+    }
+})
+
+
+app.post("/login",async (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{
+        const foundUser = await User.where({email:username}).findOne();
+
+        if(foundUser && foundUser.password === password) res.render("secrets.ejs");
+        else console.log("Username or Password is wrong");
+    }catch(err){
+        console.log(err);
+    }
 })
 
 
